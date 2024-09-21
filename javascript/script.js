995708d7ff3b140e2bd4f78da5193f46fea871f0
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-21 20:45:26 +0800

    feat: link the setInterval func to counter var

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,10 @@
 const number = document.querySelector("#number");
 let counter = 0;
+setInterval(() => {
+  if (counter == 100) {
+    clearInterval();
+  } else {
+    counter += 1;
+    number.textContent = `${counter}%`;
+  }
+}, 16);
